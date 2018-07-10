f2c8e92418e1f1fe129317219a609635c07abc15
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-07-10 16:17:42 +0200

    Refactoring urban_density

--- lib/mapotempo_classes.lua
+++ lib/mapotempo_classes.lua
@@ -1,3 +1,6 @@
+pprint=require('lib/pprint')
+Urban_density = require('lib/urban_density')
+
 local Mapotempo_classes = {}
 
 local highway_bits = Sequence {
@@ -17,6 +20,13 @@ local highway_bits = Sequence {
   track           = {false, true, false}, -- same
   -- unassigned        = {false, false, true},
   -- unassignable      = {false, false, false},
+}
+
+local landuse_bits = {
+    {false, false}, -- 1, interurban
+    {false, true}, -- 2, water_body
+    {true, false}, -- 3, urban
+    {true, true}, -- 4, urban_dense
 }
 
 -- add class information
@@ -42,6 +52,18 @@ function Mapotempo_classes.classes(profile,way,result,data)
             result.backward_classes["w1"], result.backward_classes["w2"], result.backward_classes["w3"] = unpack(w_bits)
         end
     end
+
+    -- FIXME duplicate call to speed_coef ----------------------------------------------------------------------------
+    local coef = Urban_density.speed_coef(way)
+    local max_index = 1
+    for k in pairs(coef) do
+        if coef[k] > coef[max_index] then
+            max_index = k
+        end
+    end
+
+    result.forward_classes["l1"], result.forward_classes["l2"] = unpack(landuse_bits[max_index])
+    result.backward_classes["l1"], result.backward_classes["l2"] = unpack(landuse_bits[max_index])
 end
 
 return Mapotempo_classes
