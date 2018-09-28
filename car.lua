73a1e57cef6c3842a274ea65e298ef42aa27c6c8
Alain ANDRE <wordsbybird@alain-andre.fr>
2018-09-28 19:03:09 +0200

    Apply penalties after speed reduction

--- car.lua
+++ car.lua
@@ -452,7 +452,6 @@ function process_way(profile, way, result, relations)
     WayHandlers.speed,
     WayHandlers.maxspeed,
     WayHandlers.surface,
-    WayHandlers.penalties,
 
     -- compute class labels
     WayHandlers.classes,
@@ -460,6 +459,7 @@ function process_way(profile, way, result, relations)
 
     -- set penalties after setting classes with urban density
     Mapotempo.penalties,
+    WayHandlers.penalties,
 
     -- handle turn lanes and road classification, used for guidance
     WayHandlers.turn_lanes,
