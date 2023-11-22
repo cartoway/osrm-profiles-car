2395acd630d6ba10d240b68ba9afb12003dafd19
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2023-11-22 18:47:29 +0100

    Init scooter profile

--- car.lua
+++ car.lua
@@ -24,7 +24,7 @@ function setup()
   Urban_density.assert_urban_database()
   return {
     properties = {
-      max_speed_for_map_matching      = 180/3.6, -- 180kmph -> m/s
+      max_speed_for_map_matching      = 80/3.6, -- 80kmph -> m/s
       -- For routing based on duration, but weighted for preferring certain roads
       weight_name                     = 'routability',
       -- For shortest duration without penalties for accessibility
@@ -32,29 +32,29 @@ function setup()
       -- For shortest distance without penalties for accessibility
       -- weight_name                     = 'distance',
       process_call_tagless_node      = false,
-      u_turn_penalty                 = 20,
-      continue_straight_at_waypoint  = true,
+      u_turn_penalty                 = 10,
+      continue_straight_at_waypoint  = false,
       use_turn_restrictions          = true,
       left_hand_driving              = false,
-      traffic_light_penalty          = 2,
+      traffic_light_penalty          = 1,
     },
 
     default_mode              = mode.driving,
     default_speed             = function(way) return Urban_density.default_speed(way) end, -- function
     oneway_handling           = true,
-    side_road_multiplier      = 0.8,
-    turn_penalty              = 7.5,
+    side_road_multiplier      = 0.9,
+    turn_penalty              = 4,
     speed_reduction           = 0.8, -- Not Used
     turn_bias                 = 1.075,
     cardinal_directions       = false,
 
     -- Size of the vehicle, to be limited by physical restriction of the way
-    vehicle_height = 2.0, -- in meters, 2.0m is the height slightly above biggest SUVs
-    vehicle_width = 1.9, -- in meters, ways with narrow tag are considered narrower than 2.2m
+    vehicle_height = 1.90, -- in meters, 2.0m is the height slightly above biggest SUVs
+    vehicle_width = 1, -- in meters, ways with narrow tag are considered narrower than 2.2m
 
     -- Size of the vehicle, to be limited mostly by legal restriction of the way
-    vehicle_length = 4.8, -- in meters, 4.8m is the length of large or family car
-    vehicle_weight = 2000, -- in kilograms
+    vehicle_length = 2.9, -- in meters, 2.9m is the length of a cargo scooter
+    vehicle_weight = 600, -- in kilograms
 
     -- Large vehicule
     -- Size of the vehicle, to be limited by physical restriction of the way
@@ -85,7 +85,8 @@ function setup()
 
     access_tag_whitelist = Set {
       'yes',
-      'motorcar',
+      'moped',
+      'motorcycle',
       'motor_vehicle',
       'vehicle',
       'permissive',
@@ -119,7 +120,8 @@ function setup()
     },
 
     access_tags_hierarchy = Sequence {
-      'motorcar',
+      'moped',
+      'motorcycle',
       'motor_vehicle',
       'vehicle',
       'access'
@@ -130,30 +132,26 @@ function setup()
     },
 
     restrictions = Sequence {
-      'motorcar',
+      'moped',
+      'motorcycle',
       'motor_vehicle',
       'vehicle'
     },
 
     -- change usage of some bits in classes, only to be able to return in API, could not be used internaly
     classes = Sequence {
-        'toll', 'motorway', 'track',
+        'toll',
         -- Higwhay encoding bits
         -- 'w1', 'w2', 'w3',
         -- Landuse encoding bits
         -- 'l1', 'l2',
-        'notForLargeVehicule',
-        'lowEmissionZone',
+        -- 'notForLargeVehicule',
+        -- 'lowEmissionZone',
     },
 
     -- classes to support for exclude flags
     excludable = Sequence {
         Set {'toll'},
-        Set {'motorway'},
-        Set {'toll', 'motorway'},
-        Set {'track'},
-        Set {'notForLargeVehicule'},
-        Set {'lowEmissionZone'},
     },
 
     avoid = Set {
@@ -164,7 +162,12 @@ function setup()
       'hov_lanes',
       'steps',
       'construction',
-      'proposed'
+      'proposed',
+      -- not for scooter
+      'motorway',
+      'motorway_link',
+      'trunk',
+      'trunk_link',
     },
 
     speeds = function(way) return Urban_density.speeds(way) end, -- function
@@ -172,12 +175,12 @@ function setup()
     maxspeeds = function(way, max_speed) return Urban_density.maxspeeds(way, max_speed) end, -- function
 
     service_penalties = {
-      alley             = 0.5,
-      parking           = 0.5,
+      alley             = 0.9,
+      parking           = 0.7,
       parking_aisle     = 0.5,
-      driveway          = 0.5,
-      ["drive-through"] = 0.5,
-      ["drive-thru"] = 0.5
+      driveway          = 0.9,
+      ["drive-through"] = 0.7,
+      ["drive-thru"] = 0.7
     },
 
     restricted_highway_whitelist = Set {
@@ -225,27 +228,27 @@ function setup()
       ["concrete:lanes"] = nil,
       paved = nil,
 
-      cement = 80,
-      compacted = 80,
-      fine_gravel = 80,
+      cement = 45,
+      compacted = 30,
+      fine_gravel = 20,
 
-      paving_stones = 60,
-      metal = 60,
-      bricks = 60,
+      paving_stones = 30,
+      metal = 20,
+      bricks = 20,
 
-      grass = 40,
-      wood = 40,
-      sett = 40,
-      grass_paver = 40,
-      gravel = 40,
-      unpaved = 40,
-      ground = 40,
-      dirt = 40,
-      pebblestone = 40,
-      tartan = 40,
+      grass = 20,
+      wood = 20,
+      sett = 20,
+      grass_paver = 20,
+      gravel = 20,
+      unpaved = 20,
+      ground = 20,
+      dirt = 20,
+      pebblestone = 20,
+      tartan = 20,
 
-      cobblestone = 30,
-      clay = 30,
+      cobblestone = 20,
+      clay = 20,
 
       earth = 20,
       stone = 20,
@@ -257,17 +260,17 @@ function setup()
 
     -- max speed for tracktypes
     tracktype_speeds = {
-      grade1 =  60,
-      grade2 =  40,
-      grade3 =  30,
-      grade4 =  25,
-      grade5 =  20
+      grade1 =  40,
+      grade2 =  30,
+      grade3 =  20,
+      grade4 =  15,
+      grade5 =  10
     },
 
     -- max speed for smoothnesses
     smoothness_speeds = {
-      intermediate    =  80,
-      bad             =  40,
+      intermediate    =  30,
+      bad             =  25,
       very_bad        =  20,
       horrible        =  10,
       very_horrible   =  5,
@@ -276,52 +279,52 @@ function setup()
 
     -- http://wiki.openstreetmap.org/wiki/Speed_limits
     maxspeed_table_default = {
-      urban = 50,
-      rural = 90,
-      trunk = 110,
-      motorway = 130
+      urban = 40,
+      rural = 45,
+      trunk = 45,
+      motorway = 45
     },
 
     -- List only exceptions
     maxspeed_table = {
-      ["at:rural"] = 100,
-      ["at:trunk"] = 100,
-      ["be:motorway"] = 120,
-      ["be-bru:rural"] = 70,
+      ["at:rural"] = 50,
+      ["at:trunk"] = 50,
+      ["be:motorway"] = 50,
+      ["be-bru:rural"] = 50,
       ["be-bru:urban"] = 30,
-      ["be-vlg:rural"] = 70,
-      ["by:urban"] = 60,
-      ["by:motorway"] = 110,
-      ["ch:rural"] = 80,
-      ["ch:trunk"] = 100,
-      ["ch:motorway"] = 120,
-      ["cz:trunk"] = 0,
-      ["cz:motorway"] = 0,
+      ["be-vlg:rural"] = 50,
+      ["by:urban"] = 50,
+      ["by:motorway"] = 50,
+      ["ch:rural"] = 50,
+      ["ch:trunk"] = 50,
+      ["ch:motorway"] = 50,
+      ["cz:trunk"] = 50,
+      ["cz:motorway"] = 50,
       ["de:living_street"] = 7,
-      ["de:rural"] = 100,
-      ["de:motorway"] = 0,
-      ["dk:rural"] = 80,
-      ["fr:rural"] = 80,
-      ["gb:nsl_single"] = (60*1609)/1000,
-      ["gb:nsl_dual"] = (70*1609)/1000,
-      ["gb:motorway"] = (70*1609)/1000,
-      ["nl:rural"] = 80,
-      ["nl:trunk"] = 100,
-      ['no:rural'] = 80,
-      ['no:motorway'] = 110,
-      ['pl:rural'] = 100,
-      ['pl:trunk'] = 120,
-      ['pl:motorway'] = 140,
-      ["ro:trunk"] = 100,
+      ["de:rural"] = 50,
+      ["de:motorway"] = 50,
+      ["dk:rural"] = 50,
+      ["fr:rural"] = 50,
+      ["gb:nsl_single"] = (50*1609)/1000,
+      ["gb:nsl_dual"] = (50*1609)/1000,
+      ["gb:motorway"] = (50*1609)/1000,
+      ["nl:rural"] = 50,
+      ["nl:trunk"] = 50,
+      ['no:rural'] = 50,
+      ['no:motorway'] = 50,
+      ['pl:rural'] = 50,
+      ['pl:trunk'] = 50,
+      ['pl:motorway'] = 50,
+      ["ro:trunk"] = 50,
       ["ru:living_street"] = 20,
-      ["ru:urban"] = 60,
-      ["ru:motorway"] = 110,
-      ["uk:nsl_single"] = (60*1609)/1000,
-      ["uk:nsl_dual"] = (70*1609)/1000,
-      ["uk:motorway"] = (70*1609)/1000,
-      ['za:urban'] = 60,
-      ['za:rural'] = 100,
-      ["none"] = 140
+      ["ru:urban"] = 50,
+      ["ru:motorway"] = 50,
+      ["uk:nsl_single"] = (50*1609)/1000,
+      ["uk:nsl_dual"] = (50*1609)/1000,
+      ["uk:motorway"] = (50*1609)/1000,
+      ['za:urban'] = 50,
+      ['za:rural'] = 50,
+      ["none"] = 50
     },
 
     relation_types = Sequence {
